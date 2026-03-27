93ce92f8d585115ae0eaa1a10d8e83f12451dabf
Ailten <faouzi26b@live.fr>
2026-03-27 18:38:06 +0100

    rename css/js

---
+++ JS/main_BPA.js
@@ -0,0 +1,22 @@
+
+// when html load.
+window.addEventListener('load', () => {
+
+    // get all div need to be cast as BPA-UI.
+    Array.prototype.forEach.call(
+        document.getElementsByClassName('block-programming-ui'),
+        (bpaDiv) => {
+            
+    });
+
+});
+
+
+// ---------->> functions.
+
+// function to create the BPA (from the div).
+function buildBPA(bpaDiv) {
+
+    
+
+}
