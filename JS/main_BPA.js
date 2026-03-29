130248c86a38ea8a8b8d1244d486337ecd74cf3c
Ailten <faouzi26b@live.fr>
2026-03-29 11:13:59 +0200

    garbadge

--- JS/main_BPA.js
+++ JS/main_BPA.js
@@ -1,4 +1,6 @@
 
+var myBpa;
+
 // when html load.
 window.addEventListener('load', () => {
 
@@ -7,39 +9,69 @@ window.addEventListener('load', () => {
         document.getElementsByClassName('block-programming-ui'),
         (div) => {
             
-            buildBPA(div);
+            // instantiate the main obj.
+            myBpa = new BPA(div);
+
+            // define what block allow.
+            myBpa.addBlockToMenu(BlockStart);
 
     });
 
 });
 
 
-// ---------->> functions.
+// ---------->> Class.
 
-// function to create the BPA (from the div).
-function buildBPA(div) {
-    buildMenu(div);
-    addToMenu('start');
-}
+// Block-Programming.
+class BPA {
+    bpaDiv;  // div for the canvas BPA.
+    menu;  // div for the menu who propose blocks.
+    garbage;  // div for delete block.
 
-// function to create the menu of BPA.
-function buildMenu(div) {
-    let menu = div.appendChild(document.createElement('div'));
-    menu.classList.add('menu-block');
-}
-
-
-// ---------->> block ref node.
-
-const blockType = {  // fixme : make class with parent abstract.
-    'start': {
-        createBlock: (divDest) => {
-
-        }
-    },
-    'if': {
-        createBlock: (divDest) => {
-            
-        }
+    constructor(div) {
+        this.bpaDiv = div;
+        this.menu = this.#createMenu();
+        this.garbage = this.#createGarbage();
     }
-};
+
+    // create menu (div).
+    #createMenu() {
+        let menu = this.bpaDiv.appendChild(document.createElement('div'));
+        menu.classList.add('menu-block');
+        return menu;
+    }
+    #createGarbage() {
+        let garbage = this.menu.appendChild(document.createElement('div'));
+        garbage.classList.add('garbage');
+        garbage.innerText = "poubelle";
+        return garbage;
+    }
+
+    // function to add block to menu.
+    addBlockToMenu(classBlock) {
+
+        //let block = this.menu.appendChild(document.createElement('div'));
+
+    }
+}
+
+
+// ---------- Block.
+
+class Block {
+    pos;
+
+    constructor(pos) {
+        this.pos = pos;
+    }
+}
+
+class BlockStart extends Block {
+    constructor() {
+        super([0, 0]);
+        
+    }
+}
+
+
+// events : pointerdown, pointermove, pointerup.
